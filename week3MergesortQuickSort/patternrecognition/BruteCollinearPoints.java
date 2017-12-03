65c668a1c7d74999c78e1aed5d7c8ecf64efe0d3
birdbird1117 <dapeng.han.1989@gmail.com>
2017-12-03 18:39:31 +0100

    still not pass test 10, with check that data type is immutable by testing whether each method          returns the same value, regardless of any intervening operations   * input8.txt     - failed after 3 operations involving BruteCollinearPoints     - first and last call to segments() returned different arrays

--- week3MergesortQuickSort/patternrecognition/BruteCollinearPoints.java
+++ week3MergesortQuickSort/patternrecognition/BruteCollinearPoints.java
@@ -3,105 +3,81 @@ import java.lang.*;
 import java.util.*;
 
 public class BruteCollinearPoints {
-    private    Point[] testPoints;
-    private   int cnt = 0;
-    private  LineSegment[] segs;
-    private  int segs_index = 0;
-    
-    public BruteCollinearPoints(Point[] points){
+    private Point[] testPoints;
+    private int cnt;
+    private ArrayList<LineSegment> segs;
+    //private int segs_index;
+
+    public BruteCollinearPoints(Point[] points) {
         if (points == null) {
-            throw new NullPointerException("argument is null");
+            throw new IllegalArgumentException("argument is null");
         }
-        
-        for (int i = 0; i < points.length; i++)
-        {
+        for (int i = 0; i < points.length; i++) {
             if (points[i] == null)
-                throw new NullPointerException("argument is null");
+                throw new IllegalArgumentException("argument is null");
         }
-        for (int i = 0; i < points.length; i++)
-        {
-            for (int j = i+1; j < points.length; j++)
-            {
-                if (points[i].compareTo(points[j])==0)
+        for (int i = 0; i < points.length; i++) {
+            for (int j = i + 1; j < points.length; j++) {
+                if (points[i].compareTo(points[j]) == 0)
                     throw new IllegalArgumentException("repeated points");
             }
         }
-        testPoints = points; 
-        segs = new LineSegment[testPoints.length];// too long
-    }    // finds all line segments containing 4 points
-    
-    public           int numberOfSegments(){
+        testPoints = points.clone();//.copyOf(points, points.length);
+        segs = new ArrayList<LineSegment>();// too long
+        cnt = 0;
+    } // finds all line segments containing 4 points
+
+    public int numberOfSegments() {
         return cnt;
-    }        // the number of line segments
-    
-    public LineSegment[] segments(){        
-        
+    } // the number of line segments
+
+    public LineSegment[] segments() {
+
         Point start, end;
-        for(int i = 0; i < testPoints.length; i++)
-        {
-            for(int j = i+1; j < testPoints.length; j++)
-            {
-                
-                for(int k = j+1; k < testPoints.length; k++)
-                {
-                    
-                    
-                    if (testPoints[i].slopeTo(testPoints[j]) == testPoints[i].slopeTo(testPoints[k]))
-                    {
-                        for (int l = k+1; l <testPoints.length; l++)
-                        {
-                            
-                            if (testPoints[i].slopeTo(testPoints[j]) == testPoints[i].slopeTo(testPoints[l]))
-                            {
+        for (int i = 0; i < testPoints.length; i++) {
+            for (int j = i + 1; j < testPoints.length; j++) {
+                for (int k = j + 1; k < testPoints.length; k++) {
+                    if (testPoints[i].slopeTo(testPoints[j]) == testPoints[j].slopeTo(testPoints[k])) {
+                        for (int l = k + 1; l < testPoints.length; l++) {
+                            if (testPoints[i].slopeTo(testPoints[j]) == testPoints[k].slopeTo(testPoints[l])) {
                                 // update start and end
-                                if (testPoints[i].compareTo(testPoints[j]) < 0)
-                                {
+                                if (testPoints[i].compareTo(testPoints[j]) < 0) {
                                     start = testPoints[i];
                                     end = testPoints[j];
-                                }
-                                else
-                                {
+                                } else {
                                     start = testPoints[j];
                                     end = testPoints[i];
                                 }
-                                
-                                if (start.compareTo(testPoints[k]) > 0)
-                                {
+
+                                if (start.compareTo(testPoints[k]) > 0) {
                                     start = testPoints[k];
                                 }
-                                if (end.compareTo(testPoints[k]) < 0)
-                                {
+                                if (end.compareTo(testPoints[k]) < 0) {
                                     end = testPoints[k];
                                 }
-                                
-                                if (start.compareTo(testPoints[l]) > 0)
-                                {
+
+                                if (start.compareTo(testPoints[l]) > 0) {
                                     start = testPoints[l];
                                 }
-                                if (end.compareTo(testPoints[l]) < 0)
-                                {
+                                if (end.compareTo(testPoints[l]) < 0) {
                                     end = testPoints[l];
                                 }
                                 cnt++;
-//                            StdOut.println("testpoints" +testPoints[i]);                        
-//                            StdOut.println("testpoints" +testPoints[l]);                        
-//                            StdOut.println("i-l slope" +testPoints[i].slopeTo(testPoints[l]));                        
-//                            StdOut.println("i,j,k,l, segs_index,cnt, i_l slope:" + i+j+k+l+segs_index+cnt+testPoints[i].slopeTo(testPoints[l]));                        
-                                segs[segs_index++] = new LineSegment(start,end);
-                            }   
+                                segs.add(new LineSegment(start, end));
+                            }
                         }
-                    }   
-                }        
+                    }
+                }
             }
         }
-        LineSegment[] res = new LineSegment[segs_index];
-        for (int i = 0; i < segs_index; i++)
-            res[i] = segs[i];
-        return res;
-    }                // the line segments
-    
+        LineSegment[] res = new LineSegment[segs.size()];
+        for (int i = 0; i < segs.size(); i++)
+            res[i] = segs.get(i);
+        return res.clone();
+    } // the line segments
+
     public static void main(String[] args) {
-        
+
         // read the n points from a file
         In in = new In(args[0]);
         int n = in.readInt();
@@ -111,7 +87,7 @@ public class BruteCollinearPoints {
             int y = in.readInt();
             points[i] = new Point(x, y);
         }
-        
+
         // draw the points
         StdDraw.enableDoubleBuffering();
         StdDraw.setXscale(0, 32768);
@@ -120,7 +96,7 @@ public class BruteCollinearPoints {
             p.draw();
         }
         StdDraw.show();
-        
+
         // print and draw the line segments
         BruteCollinearPoints collinear = new BruteCollinearPoints(points);
         for (LineSegment segment : collinear.segments()) {
@@ -129,6 +105,5 @@ public class BruteCollinearPoints {
         }
         StdDraw.show();
     }
-    
-    
+
 }
